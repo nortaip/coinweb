6023157e6b90be97389ca16e8aed73aa3e815fa8
Asif <67794014+nortaip@users.noreply.github.com>
2023-10-11 12:14:56 +0400

    update

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Logodd from "../imgs/logo.png";
 
 function Headers() {
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
@@ -13,13 +14,10 @@ function Headers() {
             <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                 <a href="home" className="flex items-center">
                     <img
-                        src="https://flowbite.com/docs/images/logo.svg"
-                        className="mr-3 h-6 sm:h-9"
+                        src={Logodd}
+                        className="mr-3 h-8 sm:h-20"
                         alt="Flowbite Logo"
                     />
-                    <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-                        btnsd
-                    </span>
                 </a>
                 <div className="flex items-center lg:order-2">
                     <button
